0fca22d90e142380f893a55be67e1927abf862f7
Srtian <shenruotian@kuaishou.com>
2021-02-01 17:12:27 +0800

    feat: init and fix varibles

---
+++ variables/variables6.rs
@@ -0,0 +1,11 @@
+// variables6.rs
+// Make me compile! Execute the command `rustlings hint variables6` if you want a hint :)
+
+// I AM NOT DONE
+
+const NUMBER: u32 = 3;
+fn main() {
+    // const NUMBER = 3;
+
+    println!("Number {}", NUMBER);
+}
